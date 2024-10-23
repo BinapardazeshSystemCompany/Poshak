47dc542132dcc78443e9b301b3f4308d38036c37
Amir <Amir@DESKTOP-45PT7KU>
2024-10-23 17:55:28 +0330

    - add clase engin

---
+++ پوشک/engin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace پوشک
+{
+    internal class engin
+    {
+        //*hello word*//
+    }
+}
